63338419d623d63fb31d520f00eaac9fddffc4cf
Brajan Bartoszewicz <8r4j4n@gmail.com>
2014-06-20 09:34:03 +0200

    Add database file

---
+++ viatut.sql
@@ -0,0 +1,128 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Czas wygenerowania: 20 Cze 2014, 07:30
+-- Wersja serwera: 5.6.12-log
+-- Wersja PHP: 5.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Baza danych: `viatut`
+--
+CREATE DATABASE IF NOT EXISTS `viatut` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `viatut`;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `stats`
+--
+
+CREATE TABLE IF NOT EXISTS `stats` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `force` int(11) NOT NULL DEFAULT '4',
+  `mobility` int(11) NOT NULL DEFAULT '4',
+  `intellect` int(11) NOT NULL DEFAULT '4',
+  `hp` int(11) NOT NULL DEFAULT '10',
+  `hp_max` int(11) NOT NULL DEFAULT '10',
+  `mana` int(11) NOT NULL DEFAULT '100',
+  `mana_max` int(11) NOT NULL DEFAULT '100',
+  `energy` int(11) NOT NULL DEFAULT '100',
+  `energy_max` int(11) NOT NULL DEFAULT '100',
+  `money` int(11) NOT NULL DEFAULT '0',
+  `level` int(11) NOT NULL DEFAULT '1',
+  `exp` int(11) NOT NULL DEFAULT '0',
+  `exp_to_level` int(11) NOT NULL DEFAULT '24',
+  `work_payment` int(11) NOT NULL DEFAULT '0',
+  `action_id` int(11) NOT NULL DEFAULT '0',
+  `action_end` int(11) NOT NULL DEFAULT '0',
+  `action_type` int(11) NOT NULL DEFAULT '0',
+  `lastFight` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+
+--
+-- Struktura tabeli dla tabeli `travels`
+--
+
+CREATE TABLE IF NOT EXISTS `travels` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `require_level` int(11) NOT NULL,
+  `travel_time` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Zrzut danych tabeli `travels`
+--
+
+INSERT INTO `travels` (`id`, `name`, `require_level`, `travel_time`) VALUES
+(1, 'Forest', 1, 20),
+(2, 'Dark Forest', 2, 360);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `email` varchar(256) NOT NULL,
+  `salt` varchar(32) NOT NULL,
+  `joined` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+
+--
+-- Struktura tabeli dla tabeli `users_session`
+--
+
+CREATE TABLE IF NOT EXISTS `users_session` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `hash` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `works`
+--
+
+CREATE TABLE IF NOT EXISTS `works` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) NOT NULL,
+  `rate` int(11) NOT NULL,
+  `required_level` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Zrzut danych tabeli `works`
+--
+
+INSERT INTO `works` (`id`, `name`, `rate`, `required_level`) VALUES
+(1, 'Cook', 10, 1),
+(2, 'Soldier', 15, 3);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
